20bed6ca6e245cbb4f1a2254eff46f42c96f1d51
lerojon <leroy@qq.com>
2019-07-18 22:26:09 +0800

    0718

--- kits.js
+++ kits.js
@@ -1,20 +1,18 @@
-let kits = {}
-kits.getTime = function (){
-let time = new Date();
-let y = time.getFullYear();
-let M = time.getMonth() + 1;
-let d = time.getDate();
-let h = time.getHours();
-let m = time.getMinutes();
-let s = time.getSeconds();
- M = M > 10?M:'0'+M;
- d = d > 10?d:'0'+d;
- h = h > 10?h:'0'+h;
- m = m > 10?m:'0'+m;
- s = s > 10?s:'0'+s;
-return y + '-' + M + '-' + d + ' ' + h + ':' + m + ':' + s; 
-}
-
 ;(function (){
-    
+    let kits = {}
+    kits.getTime = function (){
+    let time = new Date();
+    let y = time.getFullYear();
+    let M = time.getMonth() + 1;
+    let d = time.getDate();
+    let h = time.getHours();
+    let m = time.getMinutes();
+    let s = time.getSeconds();
+     M = M > 10?M:'0'+M;
+     d = d > 10?d:'0'+d;
+     h = h > 10?h:'0'+h;
+     m = m > 10?m:'0'+m;
+     s = s > 10?s:'0'+s;
+    return y + '-' + M + '-' + d + ' ' + h + ':' + m + ':' + s; 
+    }
 })();
